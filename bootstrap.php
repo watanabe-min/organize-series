9ceb941f8ddcd90b432952ce9e1d4b517e01ee96
Darren Ethier <darren@roughsmootheng.in>
2019-04-07 19:51:48 -0400

    implement `getVersion` function

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,10 @@
 <?php
 use OrganizeSeries\application\Root;
 use OrganizeSeries\domain\services\CoreBootstrap;
+use function OrganizeSeries\getVersion;
 
 //initialize Meta (path registration etc)
-Root::initialize(__FILE__, ORG_SERIES_VERSION);
+Root::initialize(__FILE__, getVersion());
 //register routes
 Root::container()->make(CoreBootstrap::class);
 //this is the hook that all Organize Series Extensions should hook in on.
